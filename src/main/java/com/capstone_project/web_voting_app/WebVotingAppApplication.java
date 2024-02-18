50961f71a0b87311aa48a2668debda73c9e03820
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-19 00:10:02 +0100

    first commit

---
+++ src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.capstone_project.web_voting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVotingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVotingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone_project/web_voting_app/WebVotingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone_project.web_voting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebVotingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
