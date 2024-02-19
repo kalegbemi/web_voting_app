75829801954569e634ba3d59536ceaffb1dcd230
Mary Adewunmi <physayoemma@gmail.com>
2024-02-19 08:04:36 +0100

    The VoteRepository linked to the vote entity

---
+++ src/main/java/com/capstone_project/web_voting_app/repository/VoteRepository.java
@@ -0,0 +1,16 @@
+package com.capstone_project.web_voting_app.repository;
+
+import com.capstone_project.web_voting_app.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Vote findByVoterId(long id);
+    List<Vote> findByVoter(Voter voter);
+    List<Vote> findAllByElectionId(long electionId);
+    List<Vote> findByCandidate(Candidate candidate);
+
+}
