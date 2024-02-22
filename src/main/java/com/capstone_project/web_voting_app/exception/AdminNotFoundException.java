e34163d746fe0af3801f5640bf34725eaa211ae7
Ayokunle Lawal <ayokunle.lawal@yahoo.com>
2024-02-22 22:11:57 +0100

    Added get, update and delete admin end points to my service class and controller

---
+++ src/main/java/com/capstone_project/web_voting_app/exception/AdminNotFoundException.java
@@ -0,0 +1,7 @@
+package com.capstone_project.web_voting_app.exception;
+
+    public class AdminNotFoundException extends RuntimeException{
+        public AdminNotFoundException(String message) {
+            super(message);
+        }
+}
