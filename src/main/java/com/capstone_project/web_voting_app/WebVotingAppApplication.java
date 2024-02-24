987109b4bc45df47db2ba7626b0c61485ca35285
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-24 04:14:53 +0100

    added enable scheduling to the application main class

--- src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
+++ src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
@@ -2,12 +2,23 @@ package com.capstone_project.web_voting_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class WebVotingAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebVotingAppApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(WebVotingAppApplication.class, args);
+    }
 }
