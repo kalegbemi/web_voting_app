bde589df3bc8c7e781afa8e913ab5913c5175482
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-24 04:13:12 +0100

    created a new message prototype

--- src/main/java/com/capstone_project/web_voting_app/util/MessageUtil.java
+++ src/main/java/com/capstone_project/web_voting_app/util/MessageUtil.java
@@ -2,12 +2,21 @@ package com.capstone_project.web_voting_app.util;
 
 import com.capstone_project.web_voting_app.dto.CandidateEmailDetails;
 
+import java.time.LocalDateTime;
+
 public class MessageUtil {
 
     public static String getVoterMessage(String name, String role){
         return "Hello "+ name+",\n\nYou have been registered as a "+role+" on the INGRYD VOTING APPLICATION." +
                 "\n\nFurther information will be communicated as the needs arises.\n\n\n\nThank you,\n\nSupport team.";
     }
+
+    public static String Message24HrBeforeElection(String name, String electionTitle, LocalDateTime time){
+        return "Hello "+ name+",\n\nThis is to inform / remind you that the  "+electionTitle+" election will be in the next 24 hours (" +
+                time+"). "+
+                "\n\nWe encourage you to let your vote count by participating.\n\n\n\nThank you,\n\nSupport team.";
+    }
+
 
     public static String getCandidateMessage(CandidateEmailDetails details){
         return "Hello "+ details.getName()+",\n\nYou have been registered as a "+details.getRole()+" on the INGRYD VOTING APPLICATION." +
