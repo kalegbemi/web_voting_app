1ac3475c2399df7eca7e2f42ea8aef6a67bf7c8b
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-02-26 16:24:37 +0100

    added a command line runner to instantiate / create an admin at run time.

--- src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
+++ src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
@@ -1,23 +1,39 @@
 package com.capstone_project.web_voting_app;
 
+import com.capstone_project.web_voting_app.dto.AdminRegisterRequest;
+import com.capstone_project.web_voting_app.service.AdminService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.cache.annotation.EnableCaching;
-
-import java.time.LocalDateTime;
-import java.time.Month;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 @EnableCaching
 public class WebVotingAppApplication {
+
+private Boolean hasRun = true;
+    @Bean
+
+    CommandLineRunner commandLineRunner(AdminService adminService){
+        return args -> {
+            if (!hasRun) {
+            AdminRegisterRequest adminRegisterRequest = AdminRegisterRequest.builder()
+                    .email("yusuf@yahoo.com")
+                    .fullName("Abiodun George")
+                    .password("Abiodun231@")
+                    .build();
+
+                adminService.register(adminRegisterRequest);
+                hasRun = true;
+            }
+        };
+
+    }
 
 
     public static void main(String[] args) {
