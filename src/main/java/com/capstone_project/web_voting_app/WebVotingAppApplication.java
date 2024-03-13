9426e96b04b42d8c4853278dc9294ad19b6108e1
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-03-13 02:37:52 +0100

    removing async from the main class

--- src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
+++ src/main/java/com/capstone_project/web_voting_app/WebVotingAppApplication.java
@@ -12,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 @EnableCaching
 public class WebVotingAppApplication {
 
